eaa7bc0117b176e24b8769fbe1411e831d4b80d4
Art Burov <aburov@gmail.com>
2014-01-17 18:01:19 -0500

    dp

--- code/Dynamic.js
+++ code/Dynamic.js
@@ -31,3 +31,67 @@ function print(json) {
 	}
 	console.info(res + ' - ' + count);
 }
+
+
+
+
+
+
+
+
+
+
+function allSubstrings(string) {
+	if (string.length == 1) {
+		console.log(string);
+		return;
+	}
+	else {
+		console.log(string);
+		allSubstrings(string.substring(0, string.length-1));
+		allSubstrings(string.substring(1, string.length));
+	}
+
+}
+//console.log(allSubstrings('abc', ''));
+
+
+
+
+
+
+function powersetI(set) {
+    var powerset = [[]];
+    for (var i=0; i < set.length; i++) 
+        for (var j = 0, len = powerset.length; j < len; j++) 
+            powerset.push(powerset[j].concat(set[i]));
+    return powerset;
+}
+ 
+var res = powersetI([1,2,3]);
+ 
+console.log(res.join('\n '));
+
+
+
+
+
+
+
+
+
+
+
+
+
+function powersetR(set) {
+	if (!set) return;
+    for (var i=0; i < set.length; i++) 
+    	powersetR(set.substring(0,i) + set.substring(i+1, set.length));
+    console.log(set);
+}
+ 
+var res = powersetR('1234');
+ 
+//console.log(res.join('\n '));
+
